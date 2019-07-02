71947d3d90796dd59f9e6f1db064871be3774b27
Tanzim Mokammel <mtanzim@gmail.com>
2019-07-02 11:58:48 +0200

    finish p4

--- hw4/hw4.rkt
+++ hw4/hw4.rkt
@@ -4,16 +4,18 @@
 (provide (all-defined-out)) ;; so we can put tests in a second file
 
 ;; put your code below
+(define ones (lambda () (cons 1 ones)))
+(define powers-of-two
+  (letrec ([f (lambda (x) (cons x (lambda () (f (* x 2)))))])
+    (lambda () (f 2))))
 
 (define (sequence low high stride)
     (if (> low high) 
         null
         (cons low (sequence (+ low stride) high stride))
-    )
-)
+    ))
 (define (string-append-map xs suffix)
-    (map (lambda (i) (string-append i suffix)) xs)
-)
+    (map (lambda (i) (string-append i suffix)) xs))
 
 (define (list-nth-mod xs num)
   (define (helper cur-xs cur-count)
@@ -26,5 +28,11 @@
       (error "list-nth-mod: empty list")
       (if (< num 0)
           (error "list-nth-mod: negative number")
-          (helper xs 0)))
-)
+          (helper xs 0))))
+
+(define (stream-for-n-steps s n)
+  (define (helper cur-xs cur-count cur-s)
+    (if (= cur-count n)
+        cur-xs
+        (helper (append cur-xs (list(car (cur-s))) ) (+ 1 cur-count)  (cdr (cur-s))  )))
+  (helper  '() 0 s))
--- hw4/hw4test.rkt
+++ hw4/hw4test.rkt
@@ -13,6 +13,12 @@
 
 ;; Helper functions
 (define ones (lambda () (cons 1 ones)))
+(define powers-of-two
+  (letrec ([f (lambda (x) (cons x (lambda () (f (* x 2)))))])
+    (lambda () (f 2))))
+
+
+
 (define a 2)
 
 (define tests
@@ -39,7 +45,10 @@
     (check-equal? (list-nth-mod (list 45 56 66 63 2344) 6) 56 "list-nth-mod test")
    
    ; stream-for-n-steps test
-   ;(check-equal? (stream-for-n-steps ones 2) (list 1 1) "stream-for-n-steps test")
+   (check-equal? (stream-for-n-steps ones 2) (list 1 1) "stream-for-n-steps test")
+   (check-equal? (stream-for-n-steps ones 1) (list 1) "stream-for-n-steps test")
+   (check-equal? (stream-for-n-steps ones 0) '() "stream-for-n-steps test")
+   (check-equal? (stream-for-n-steps powers-of-two 4) (list 2 4 8 16) "stream-for-n-steps test")
    
    ; funny-number-stream test
    ;(check-equal? (stream-for-n-steps funny-number-stream 16) (list 1 2 3 4 -5 6 7 8 9 -10 11 12 13 14 -15 16) "funny-number-stream test")
